cef5e3d2b73b9ddee67c5768cd1c629105ece901
rahul dwivedi <rahul.dwivedi@icims.com>
2024-03-20 18:54:09 +0530

    feat: adding ellipsis to add more options such as edit/delete

---
+++ src/components/popupForVerticalEllipsis.js
@@ -0,0 +1,33 @@
+import React, {useState, useEffect} from 'react';
+import './verticalEllipsis.css'
+import { HiEllipsisVertical } from "react-icons/hi2";
+import useHandleVerticalEllipsisVisibility from "../customHooks/customHooksForVerticalEllipsis";
+
+function PopupForVerticalEllipsis(_id) {
+
+  const [elementRef, showVisibility, setShowVisibility] = useHandleVerticalEllipsisVisibility(false)
+
+  return (
+    <>
+    <div ref={elementRef} style={{marginLeft: 'auto', position: 'relative'}}>
+        <HiEllipsisVertical />
+
+        {
+            showVisibility ? (<div style={{position: 'absolute', background: '#fff', right: 0, bottom: 0}}>
+            <div className='mainSection'>
+                <div className='menu'>
+                    Edit
+                </div>
+                <div className='menu'>
+                    Delete
+                </div>
+            </div>
+        </div>) : ''
+        }
+        
+    </div>
+    </>
+  );
+}
+
+export default PopupForVerticalEllipsis;
---
+++ src/customHooks/customHooksForVerticalEllipsis.js
@@ -0,0 +1,26 @@
+import React, {useState, useRef, useEffect} from "react";
+
+function useHandleVerticalEllipsisVisibility(visibility){
+    const elementRef = useRef(null);
+    const [showVisibility, setShowVisibility] = useState(false);
+
+    const handleClickOutside = (e)=>{
+        debugger;
+        if(elementRef.current && !elementRef.current.contains(e.target)){
+            setShowVisibility(false);
+        }else{
+            setShowVisibility(true)
+        }
+    }
+    useEffect(()=>{
+        document.addEventListener('click', handleClickOutside);
+        return()=>{ 
+            document.removeEventListener('click', handleClickOutside);
+        }
+    },[])
+
+    return [elementRef, showVisibility, setShowVisibility]
+
+}
+
+export default useHandleVerticalEllipsisVisibility;
